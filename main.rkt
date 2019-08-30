a050237a630dd6f9a4e68de9d8b5616d2424bcbd
Leif Andersen <leif@leifandersen.net>
2019-08-30 13:42:52 -0400

    Add checking for consistent DPI.

--- main.rkt
+++ main.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 #|
-   Copyright 2018 Leif Andersen
+   Copyright 2018-2019 Leif Andersen
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,6 +16,18 @@
    limitations under the License.
 |#
 
+(require (for-syntax racket/base))
+
+;; Make sure the "png-image" package is installed
+(begin-for-syntax
+  (require pkg
+           pkg/lib)
+  (define package "png-image")
+  (parameterize ([current-input-port (open-input-bytes #"")])
+    (unless (hash-has-key? (installed-pkg-table) package)
+      (displayln "Installing Required Packages...")
+      (pkg-install-command #:deps 'search-auto package))))
+
 (require racket/gui/base
          racket/class
          racket/draw
@@ -24,8 +36,11 @@
          racket/path
          pict
          racket/pretty
-         mrlib/path-dialog)
+         mrlib/path-dialog
+         png-image)
 
+;; Path-String -> (Listof Path-String)
+;; Directory Path to list of pngs in directory
 (define (collect-images path)
   (for/list ([i (in-list (directory-list path #:build? #t))]
              #:when (let ()
@@ -33,6 +48,16 @@
                       (and the-path
                            (string-ci=? (bytes->string/locale the-path) ".png"))))
     i))
+
+;; Path-String -> (Pair Number Number)
+;; Image Path to X and Y DPI for image
+(define (image->dpi image)
+  (define DPM->DPI #e39.37)
+  (let* ([acc (png->hash image)]
+         [acc (hash-ref acc 'pHYs)]
+         [acc (hash-ref acc 'data)])
+    (cons (/ (integer-bytes->integer acc 0 #t 0 4) DPM->DPI)
+          (/ (integer-bytes->integer acc 0 #t 4 8) DPM->DPI))))
 
 (define (images->pdf-order images)
   images)
@@ -53,9 +78,38 @@
                      (rest (rest forward))
                      (rest (rest backward)))))))
 
+;; (Listof Path-String) -> (Or #f (Pair (List...) (List ...))
+;; Checks for a consistend width/height/dpi for all images.
+;; False if they match, otherwise a structure of the problem.
+(define (validate-png-list images)
+  (define-values (width height dpi-x dpi-y)
+    (cond
+      [(and (list? images)
+            (not (null? images)))
+       (define f (bitmap (first images)))
+       (define dpi (image->dpi (first images)))
+       (values (pict-width f) (pict-height f) (car dpi) (cdr dpi))]
+      [else (values #f #f #f #f)]))
+  
+    (for/fold ([ret #f])
+              ([i (in-list images)])
+      #:break ret
+      (define f (bitmap i))
+      (define dpi (image->dpi i))
+      (or ret
+          (if (and (= width (pict-width f))
+                   (= height (pict-height f))
+                   (= dpi-x (car dpi))
+                   (= dpi-y (cdr dpi)))
+              #f
+              (cons (list (first images) width height dpi-x dpi-y)
+                    (list i (pict-width f) (pict-height f) (car dpi) (cdr dpi)))))))
+
+;; (Listof Path-String) [Path-String] -> Void
+;; Takes an ordered list of png paths, concatenates them into a PDF
 (define (image-list->pdf images [output #f])
 
-  (define-values (width height)
+  (define-values (width height dpi-x dpi-y)
     (cond
       [(and (list? images)
             (not (null? images)))
@@ -106,8 +160,31 @@
                        (set! image-folder new-folder)
                        (unless (modulo (length (collect-images new-folder)) 4)
                          (set! image-folder #f)
-                         (message-box "Invalid Folder"
-                                      "Images not a multiple of four."))
+                         (message-box "WARNING: Invalid Folder"
+                                      "WARNING: Images not a multiple of four."
+                                      #f
+                                      '(ok caution)))
+                       (define invalid? (validate-png-list (collect-images new-folder)))
+                       (when invalid?
+                         (message-box
+                          "WARNING: Inconsistent Images"
+                          (format (string-append "WARNING: Image ~a and ~a are not compatable:\n\n"
+                                                 "~a: (pixels: ~ax~a, dpi: ~ax~a)\n"
+                                                 "~a: (pixels: ~ax~a, dpi: ~ax~a)")
+                                              (first (car invalid?))
+                                              (first (cdr invalid?))
+                                              (first (car invalid?))
+                                              (second (car invalid?))
+                                              (third (car invalid?))
+                                              (exact->inexact (fourth (car invalid?)))
+                                              (exact->inexact (fifth (car invalid?)))
+                                              (first (cdr invalid?))
+                                              (second (cdr invalid?))
+                                              (third (cdr invalid?))
+                                              (exact->inexact (fourth (cdr invalid?)))
+                                              (exact->inexact (fifth (cdr invalid?))))
+                          #f
+                          '(ok caution)))
                        (if image-folder
                            (send file-label set-label
                                  (format "Current Location: ~a" image-folder))
